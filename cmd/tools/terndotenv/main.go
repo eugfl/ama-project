9e04ad77d6c7145a696a0b0bd1e4a96f216e88e4
Gabriel Figueiredo <eugfl@hotmail.com>
2024-08-07 16:03:31 -0300

    inital commit

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    var stderr bytes.Buffer
+    cmd := exec.Command(
+        "tern",
+        "migrate",
+        "--migrations",
+        "./internal/store/pgstore/migrations",
+        "--config",
+        "./internal/store/pgstore/migrations/tern.conf",
+    )
+    cmd.Stderr = &stderr
+
+    if err := cmd.Run(); err != nil {
+        log.Println("Error running tern migrate:", stderr.String())
+        panic(err)
+    }
+}
